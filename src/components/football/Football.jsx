6059242a6b22169256a5bb8ec5cb74ed99a13783
Pranav Murali <pranavmurali2001@gmail.com>
2021-05-01 16:42:07 +0530

    :clock3: react three fiber

--- src/components/football/Football.jsx
+++ src/components/football/Football.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import ReactDOM from 'react-dom'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import "./styles.css"
+
+
+function Box(props) {
+  // This reference will give us direct access to the THREE.Mesh object
+  const mesh = useRef()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
 const Football = () => {
     return (
-        <div>
-            football
-        </div>
+        <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+        </Canvas>
     )
 }
 
-export default Football
+export default Football;
